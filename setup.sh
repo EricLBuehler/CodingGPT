e38e6dbdb49cccde6a617e319a73040e60605ed1
EricLBuehler <ericlbuehler@gmail.com>
2023-04-26 12:13:01 +0000

    Fix typo

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ git clone https://github.com/EricLBuehler/CodingGPT.git
 cd CodingGPT
 git config --global credential.helper store
 git config --global user.email  "ericlbuehler@gmail.com"
-t config --global user.name "EricLBuehler"
+git config --global user.name "EricLBuehler"
