c9392088980101bb4b6c95d7bac4a32b87d3e88d
EricLBuehler <ericlbuehler@gmail.com>
2023-04-26 11:53:45 +0000

    Update setup

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/EricLBuehler/CodingGPT.git
 cd CodingGPT
 git config --global credential.helper store
+git config --global user.email  "ericlbuehler@gmail.com"
+t config --global user.name "EricLBuehler"
