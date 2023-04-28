b45249291cd3c27df5d2c78d7e50c82e82ebbf1e
EricLBuehler <ericlbuehler@gmail.com>
2023-04-28 09:56:52 +0000

    Train v3

--- data/scraper.py
+++ data/scraper.py
@@ -4,7 +4,7 @@ import tqdm
 def clean_text(text: str) -> str: #Strip text
     return text.strip()
 
-def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, max_allow_len: int = 1024) -> int:
+def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, max_allow_len: int = 512) -> int:
     print("Scraping", subreddit)
     hot_posts = reddit.subreddit(subreddit).hot(limit = 10000)
     maxlen = -1
@@ -17,14 +17,16 @@ def scrape_subreddit(reddit: praw.Reddit, data: list, subreddit: str, max_allow_
         else:
             question += clean_text(post.title)
 
-        if len(list(bytes(post.selftext, "utf8"))) < 512:
+        if len(list(bytes(post.selftext, "utf8"))) < max_allow_len//2:
             question += " "
             question += clean_text(post.selftext)
         question += "\n"
-        
+
         for top_level_comment in reddit.submission(id=post.id).comments:
             if isinstance(top_level_comment, praw.models.MoreComments):
                 for comment in top_level_comment.comments():
+                    if not hasattr(comment, "body"):
+                        continue
                     if comment.body == "[removed]" or comment.stickied:
                         continue
                     res = question + clean_text(comment.body) + "\0"
@@ -52,96 +54,47 @@ reddit = praw.Reddit(client_id="fO05Zgvq53-yoAPrz-Yesw", client_secret='9rkeMCec
 data = []
 maxlen = -1
 
-try:
-    max = scrape_subreddit(reddit, data, "AskScience") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
+subreddits = [
+    "AskScience",
+    "LearnProgramming",
+    "LearnPython",
+    "cpp_questions",
+    "javahelp",
+    "askprogramming",
+    "java",
+    "CodingHelp",
+    "AskProgrammers",
+    "datascience",
+    "flask",
+    "AskStatistics",
+    "AskHistorians", #v2
+    "explainlikeimfive",
+    "cscareerquestions", #v3
+    "questions",
+    "rust",
+    "C_programming", #v4
+    "pythonhelp",
+    "math",
+    "AskScienceDiscussion",
+]
 
-try:
-    max = scrape_subreddit(reddit, data, "LearnProgramming") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
+for i, subreddit in enumerate(subreddits):
+    print(f"{i+1}/{len(subreddits)}")
+    try:
+        max = scrape_subreddit(reddit, data, subreddit) 
+        if max > maxlen:
+            maxlen = max
 
-try:
-    max = scrape_subreddit(reddit, data, "LearnPython") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "cpp_questions") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "javahelp") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "askprogramming") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "java") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "CodingHelp") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "AskProgrammers") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "datascience") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "flask") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
-
-try:
-    max = scrape_subreddit(reddit, data, "AskStatistics") 
-    if max > maxlen:
-        maxlen = max
-except:
-    pass
+        print(len(data))
+    except Exception as e:
+        print(e)
 
 try:
     print(len(data),"data points")
     print("Max length:",maxlen)
     print("Average length:", sum([len(item) for item in data])/len(data))
-except:
-    pass
+except Exception as e:
+    print(e)
 
-with open("reddit_scrape.txt", "w") as f:
+with open("reddit_scrape_v4.txt", "w") as f:
     f.write("\n".join(data))
